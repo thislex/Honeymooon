4a46f1c8828a8e9542a29ae7aa7a633c3a09ce77
Lexter Tapawan <lexter.tapawan@gmail.com>
2023-12-12 09:41:39 +0000

    Initial Commit

---
+++ Honeymooon/HoneymooonApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoneymooonApp.swift
+//  Honeymooon
+//
+//  Created by Lexter Tapawan on 12/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct HoneymooonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
